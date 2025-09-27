bcadba5fcac3ffa9b8cf908d6157087d42f01f99
AzizJuliansyah <123914220+AzizJuliansyah@users.noreply.github.com>
2025-09-27 10:21:22 +0700

    Create form.blade.php

---
+++ template/form.blade.php
@@ -0,0 +1,9 @@
+<div class="space-y-2">
+    <label class="flex items-center text-sm font-semibold text-gray-700">
+        -- Judul Input -- 
+    </label>
+    <div class="relative">
+        <input type="text" name="" placeholder=""
+            class="w-full border border-gray-300 rounded-lg px-4 py-3 bg-gray-50 text-gray-900 placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 transition duration-200">
+    </div>
+</div>
