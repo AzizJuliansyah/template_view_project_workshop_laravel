84ed0b3dd3bc8eda8afc5803229be837190f0d96
AzizJuliansyah <123914220+AzizJuliansyah@users.noreply.github.com>
2025-09-27 10:33:54 +0700

    v1.1

--- template/form.blade.php
+++ template/form.blade.php
@@ -3,7 +3,11 @@
         -- Judul Input -- 
     </label>
     <div class="relative">
-        <input type="text" name="" placeholder=""
-            class="w-full border border-gray-300 rounded-lg px-4 py-3 bg-gray-50 text-gray-900 placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 transition duration-200">
+        <input
+            type="text"
+            name=""
+            placeholder=""
+            class="w-full border border-gray-300 rounded-lg px-4 py-3 bg-gray-50 text-gray-900 placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 transition duration-200"
+        >
     </div>
 </div>
